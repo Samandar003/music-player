2e0afea2f7eebaef2e05b471e7d55bf8626c841a
Samandar003 <samandar200527@gmail.com>
2022-02-26 18:43:04 +0500

    added music playing func

--- music/models.py
+++ music/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Song(models.Model):
-    title= models.TextField()
-    artist= models.TextField()
+    title= models.CharField(max_length=100)
+    artist= models.CharField(max_length=100)
     image= models.ImageField()
     audio_file = models.FileField(blank=True,null=True)
     audio_link = models.CharField(max_length=200,blank=True,null=True)
-    duration=models.CharField(max_length=20)
+    duration=models.PositiveSmallIntegerField()
     paginate_by = 2
 
     def __str__(self):
--- music/urls.py
+++ music/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('delete/<str:pk>/', views.deleteMusic, name='delete')
 ]
 
 
